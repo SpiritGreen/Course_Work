4738acdd8d68c1c9f4191008350f55d7c14f0b07
SpiritGreen <79371068+SpiritGreen@users.noreply.github.com>
2022-04-29 22:23:19 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+
+namespace Course_Work
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Message.FillDictionary(Message.keyValues_ru, Message.letters_ru);
+            Message.FillDictionary(Message.keyValues_RU, Message.letters_RU);
+
+        }
+
+        // Кнопка для выбора текстового файла:
+        private void SelectFile_Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (openFile.ShowDialog() == true)
+            {
+                using (StreamReader readText = new StreamReader(openFile.FileName, System.Text.Encoding.UTF8))
+                {
+                    InputBox.Text = readText.ReadToEnd();
+                }
+            }
+        }
+
+        //  Кнопка для расшифровки сообщения:
+        private void Decode_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (MainWindow.KeyValidation(KeyBox.Text))
+            {
+                Message message = new Message(InputBox.Text, KeyBox.Text.ToLower());
+                ResultBox.Text = message.Decode();
+            }
+        }
+        // Кнопка для шифровки сообщения:
+        private void Encode_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (MainWindow.KeyValidation(KeyBox.Text))
+            {
+                Message message = new Message(InputBox.Text, KeyBox.Text.ToLower());
+                ResultBox.Text = message.Encode();
+            }
+        }
+
+        // Кнопка для сохранения файла:
+        private void SaveFile_Button_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Filter = "Text file (*.txt)|*.txt";
+            if (saveFile.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFile.FileName, ResultBox.Text);
+            }
+        }
+
+        // Валидация ключа:
+        public static bool KeyValidation(string key)
+        {
+            if (string.IsNullOrEmpty(key) || string.IsNullOrWhiteSpace(key))
+            {
+                MessageBoxImage icon = MessageBoxImage.Error;
+                MessageBoxButton button = MessageBoxButton.OK;
+                MessageBox.Show("Ключ не введён", "Ошибка", button, icon);
+                return false;
+            }
+            foreach (var c in key)
+            {
+                if (!Message.keyValues_ru.ContainsKey(c) && !Message.keyValues_RU.ContainsKey(c))
+                {
+                    MessageBoxImage icon = MessageBoxImage.Error;
+                    MessageBoxButton button = MessageBoxButton.OK;
+                    MessageBox.Show("Ключ должен содержать только буквы русского алфавита", "Ошибка", button, icon);
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Message.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course_Work
+{
+    public class Message
+    {
+        public static string letters_ru = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+        public static string letters_RU = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+        public static Dictionary<char, int> keyValues_ru = new Dictionary<char, int>();
+        public static Dictionary<char, int> keyValues_RU = new Dictionary<char, int>();
+        public string Text { get; set; }
+        public string Key { get; set; }
+        public Message(string text, string key)
+        {
+            Text = text;
+            Key = key;
+        }
+        public static void FillDictionary(Dictionary<char, int> dictionary, string s)
+        {
+            for (int i = 0; i < s.Length; i++)
+            {
+                dictionary.Add(s[i], i);
+            }
+        }
+        public string Decode()
+        {
+            string decoded_message = "";
+            int shiftCount = 0;
+            int shift;
+            foreach (char c in Text)
+            {
+                if (letters_ru.Contains(c))
+                {
+                    shift = keyValues_ru[c];
+                    char letter = Key[shiftCount];
+                    shift -= keyValues_ru[letter];
+                    if (shift < 0) 
+                    {
+                        shift += 33; 
+                    }
+                    decoded_message += keyValues_ru.ElementAt(shift).Key;
+                    shiftCount++;
+                    if (shiftCount == Key.Length)
+                    {
+                        shiftCount = 0;
+                    }
+                }
+                else if (letters_RU.Contains(c))
+                {
+                    shift = keyValues_RU[c];
+                    shift -= keyValues_ru[Key[shiftCount]];
+                    if (shift < 0)
+                    {
+                        shift += 33;
+                    }
+                    decoded_message += keyValues_RU.ElementAt(shift).Key;
+                    shiftCount++;
+                    if (shiftCount == Key.Length)
+                    {
+                        shiftCount = 0;
+                    }
+                }
+                else
+                {
+                    decoded_message += c;
+                }
+            }
+
+            return decoded_message;
+        }
+        public string Encode()
+        {
+            string encoded_message = "";
+            int shiftCount = 0;
+            int shift;
+            foreach (char c in Text)
+            {
+                if (letters_ru.Contains(c))
+                {
+                    shift = keyValues_ru[c];
+                    char letter = Key[shiftCount];
+                    shift += keyValues_ru[letter];
+                    if (shift > 32)
+                    {
+                        shift -= 33;
+                    }
+                    encoded_message += keyValues_ru.ElementAt(shift).Key;
+                    shiftCount++;
+                    if (shiftCount == Key.Length)
+                    {
+                        shiftCount = 0;
+                    }
+                }
+                else if (letters_RU.Contains(c))
+                {
+                    shift = keyValues_RU[c];
+                    shift += keyValues_ru[Key[shiftCount]];
+                    if (shift < 0)
+                    {
+                        shift -= 33;
+                    }
+                    encoded_message += keyValues_RU.ElementAt(shift).Key;
+                    shiftCount++;
+                    if (shiftCount == Key.Length)
+                    {
+                        shiftCount = 0;
+                    }
+                }
+                else
+                {
+                    encoded_message += c;
+                }
+            }
+            return encoded_message;
+        }
+    }
+}
